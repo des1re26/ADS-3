cf8de7d837ef31888dbd8e25a55b38f2a3f45268
des1re26 <des1re26@mail.ru>
2026-04-01 20:42:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,7 +8,7 @@ static std::unordered_map<uint64_t, unsigned int> lenCache;
 uint64_t collatzMaxValue(uint64_t num) {
     uint64_t maxVal = num;
     uint64_t n = num;
-    
+
     while (n != 1) {
         if (n % 2 == 0) {
             n = n / 2;
@@ -19,19 +19,19 @@ uint64_t collatzMaxValue(uint64_t num) {
             maxVal = n;
         }
     }
-    
+
     return maxVal;
 }
 
 unsigned int collatzLen(uint64_t num) {
     if (num == 1) return 1;
-    
+
     auto it = lenCache.find(num);
     if (it != lenCache.end()) return it->second;
-    
+
     unsigned int len = 0;
     uint64_t n = num;
-    
+
     while (n != 1 && lenCache.find(n) == lenCache.end()) {
         len++;
         if (n % 2 == 0) {
@@ -40,13 +40,13 @@ unsigned int collatzLen(uint64_t num) {
             n = 3 * n + 1;
         }
     }
-    
+
     if (lenCache.find(n) != lenCache.end()) {
         len += lenCache[n];
     } else {
         len += 1;
     }
-    
+
     lenCache[num] = len;
     return len;
 }
@@ -56,7 +56,7 @@ unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t rbound) {
     unsigned int bestNum = 0;
     unsigned int bestLen = 0;
-    
+
     for (uint64_t num = lbound; num <= rbound; ++num) {
         unsigned int len = collatzLen(num);
         if (len > bestLen) {
@@ -64,10 +64,10 @@ unsigned int seqCollatz(unsigned int *maxlen,
             bestNum = static_cast<unsigned int>(num);
         }
     }
-    
+
     if (maxlen != nullptr) {
         *maxlen = bestLen;
     }
-    
+
     return bestNum;
 }
